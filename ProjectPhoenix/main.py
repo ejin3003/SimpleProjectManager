6feec3ff24e606d39720dd118e0da6a59dd8cedf
Ejin3003 <ejin3003@gmail.com>
2020-05-12 22:08:58 -0400

    Definition Mastery

--- ProjectPhoenix/main.py
+++ ProjectPhoenix/main.py
@@ -1,5 +1,7 @@
 from ProjectPhoenix.Tools.act_rep_cleaner import *
+from ProjectPhoenix.Tools.graph_config import *
 
 path = r"C:\Users\tyson\OneDrive\Desktop\Apr 2020 Act Rep.xlsx"
 df_daily_totals = act_report(path)
 
+graph_daily_trip_totals(df_daily_totals)

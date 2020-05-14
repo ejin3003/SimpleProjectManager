0d2a98d89c59c9c06352f2ba40e8add3d7dfcfb9
Ejin <ejin3003@gmail.com>
2020-05-14 17:26:15 -0400

    Completed Total Trips

--- ProjectPhoenix/main.py
+++ ProjectPhoenix/main.py
@@ -1,7 +1,10 @@
-from ProjectPhoenix.Tools.act_rep_cleaner import *
-from ProjectPhoenix.Tools.graph_config import *
+# from ProjectPhoenix.Tools.act_rep_cleaner import *
+# from ProjectPhoenix.Tools.graph_config import *
+from Tools.act_rep_cleaner import *
+from Tools.graph_config import *
 
-path = r"C:\Users\tyson\OneDrive\Desktop\Apr 2020 Act Rep.xlsx"
+
+path = r"C:\Users\ejin3\OneDrive\Desktop\Apr 2020 Act Rep.xlsx"
 df_daily_totals = act_report(path)
 
 graph_daily_trip_totals(df_daily_totals)
